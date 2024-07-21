e7573877e49112d2960654e6ed431da3f70b8906
lucasfregonezi <lucasfregonezi@gmail.com>
2024-07-21 12:36:33 -0300

    New feature - Resource created to show the elapsed time of the application in real time in the page title

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,37 +1,140 @@
 import { Play } from "phosphor-react";
+import {useForm} from 'react-hook-form';
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from 'zod';
+import { useEffect, useState } from "react";
+import { differenceInSeconds } from "date-fns";
+
 import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
 
+
+const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1, 'Informe a tarefa'),
+    minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ser de no minimo 5 minutos')
+    .max(180, 'O ciclo precisa ser de no maximo 180 minutos'),
+})
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
+interface Cycle {
+    id: string
+    task: string
+    minutesAmount: number
+    startDate: Date
+}
+
 export function Home(){
+
+    const [cycles, setCycles] = useState<Cycle[]>([])
+    const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+
+    const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+    
+    const {register, handleSubmit, watch, reset} = useForm<NewCycleFormData>({
+        resolver: zodResolver(newCycleFormValidationSchema),
+        defaultValues: {
+            task: '',
+            minutesAmount: 5
+        }
+    })
+    
+    const activeCycle = cycles.find(cycle => cycle.id === activeCycleId)
+
+    useEffect(() => {
+        let interval: number;
+        if(activeCycle) {
+            interval = setInterval(() => {
+                setAmountSecondsPassed(differenceInSeconds(new Date(), activeCycle.startDate))
+            }, 1000)
+        }
+
+        return () => {
+            clearInterval(interval)
+        }
+    },[activeCycle])
+
+    function handleCreateNewCycle(data: NewCycleFormData){
+
+        const id = String(new Date().getTime());
+
+        const newCycle: Cycle = {
+            id,
+            task: data.task,
+            minutesAmount: data.minutesAmount,
+            startDate: new Date()
+        }
+        setCycles((state) =>[...state, newCycle])
+        setActiveCycleId(id)
+        setAmountSecondsPassed(0)
+        reset();
+    }
+
+    
+
+    const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
+    const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
+
+    const minutesAmount = Math.floor(currentSeconds / 60)
+    const secondsAmount = currentSeconds % 60
+
+    const minutes = String(minutesAmount).padStart(2, '0')
+    const seconds = String(secondsAmount).padStart(2, '0')
+
+    useEffect(() => {
+        if(activeCycle){
+            document.title = `Ignite Task Timer -> ${minutes}:${seconds}`
+        }
+    },[minutes, seconds, activeCycle])
+
+    const task = watch('task');
+    const isSubmitDisabled = !task;
+    
+
     return (
         <HomeContainer>
 
-            <form>
+            <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
                 <FormContainer>
                     <label>Vou trabalhar em</label>
-                    <TaskInput id="task" list="task-suggestion" placeholder="Dê um nome para seu projeto" />
+                    <TaskInput 
+                        id="task" 
+                        list="task-suggestion" 
+                        placeholder="Dê um nome para seu projeto"
+                        {...register('task')}
+                    />
 
                     <datalist id="task-suggestion">
-                        <option value="Projeto 1" />
-                        <option value="Projeto 2" />
-                        <option value="Projeto 3" />
-                        <option value="Projeto 4" />
+                        <option value="Estudar React JS" />
+                        <option value="Estudar Node" />
+                        <option value="Estudar React Native" />
+                        <option value="Estudar C#" />
                     </datalist>
 
                     <label htmlFor="">durante</label>
-                    <MinutesAmountInput type="number" id="minutesAmount" step={5} min={5} max={60} placeholder="00" />
+                    <MinutesAmountInput 
+                        type="number" 
+                        id="minutesAmount" 
+                        step={5} 
+                        min={5} 
+                        max={180} 
+                        placeholder="00"
+                        {...register('minutesAmount', {valueAsNumber: true})} 
+                    />
 
                     <span>minutos.</span>
                 </FormContainer>
 
                 <CountdownContainer>
-                    <span>0</span>
-                    <span>0</span>
+                    <span>{minutes[0]}</span>
+                    <span>{minutes[1]}</span>
                     <Separator>:</Separator>
-                    <span>0</span>
-                    <span>0</span>
+                    <span>{seconds[0]}</span>
+                    <span>{seconds[1]}</span>
                 </CountdownContainer>
 
-                <StartCountdownButton type="submit"><Play size={24}/>Começar</StartCountdownButton> 
+                <StartCountdownButton disabled={isSubmitDisabled} type="submit"><Play size={24}/>Começar</StartCountdownButton> 
 
             </form>
 
